53e551ac285b6f7925bea240443c53975bb6282b
Harshit <bkharshitbk@gmail.com>
2021-09-21 22:58:34 +0530

    add models

---
+++ models/user.js
@@ -0,0 +1,48 @@
+const moongoose = require("mongoose");
+// const multer = require('multer');
+// const path = require('path');
+// const AVATAR_PATH = path.join('/uploads/users/avatars');
+
+const userSchema = new moongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+
+        password: {
+            type: String,
+            required: true,
+        },
+
+        // avatar: {
+        //     type: String
+        // }
+    },
+    {
+        timestamps: true,
+    }
+);
+
+// let storage = multer.diskStorage({
+//     destination: function(req, file, cb){
+//         cb(null, path.join(__dirname, '..', AVATAR_PATH));
+//     },
+//     filename: function(req, file, cb){
+//         cb(null, file.fieldname + '-' + Date.now());
+//     }
+// });
+
+// userSchema.statics.uploadAvatar = multer({storage: storage}).single('avatar');
+// userSchema.statics.avatarPath = AVATAR_PATH;
+
+const User = moongoose.model("User", userSchema);
+
+module.exports = User;
