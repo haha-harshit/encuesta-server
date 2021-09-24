de06c856ccaeb2e954fbfecf8d03692b2fb61616
Harshit <bkharshitbk@gmail.com>
2021-09-25 01:58:50 +0530

    controller for create-user

--- controllers/api/auth.js
+++ controllers/api/auth.js
@@ -8,13 +8,44 @@ module.exports.auth = (req, res) => {
 };
 
 // create user action
-// module.exports.create_user = function (req, res) {
-//     const errors = validationResult(req);
-//     if (!errors.isEmpty()) {
-//         return res.status(400).json({ erros: errors.message() });
-//     }
-//     try {
-//         // check if user email is already registered
-//         User.findOne({ email: req.body.email });
-//     } catch (error) {}
-// };
+module.exports.create_user = function (req, res) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    // check if user email is already registered
+    User.findOne({ email: req.body.email }, function (err, user) {
+        // technical error
+        if (err) {
+            console.log(err);
+            // TO DO: Add return path
+            return;
+        }
+
+        // for user's email successfully found in database---->> ALREADY TAKEN!
+        if (user) {
+            console.log("E-mail already registered!");
+            // TO DO: Add return path
+            return;
+        }
+
+        // if not found
+        if (!user) {
+            User.create(req.body, function (err, user) {
+                if (err) {
+                    console.log("Error in creating account");
+                    console.log(err);
+                    // TO DO: Add return path
+                    return;
+                }
+                console.log("Account created successfully!", user);
+                // TO DO: Add return path
+                return;
+            });
+        } else {
+            console.log("Error in creating account!");
+            return res.redirect("back");
+        }
+    });
+};
