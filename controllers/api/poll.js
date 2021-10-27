eba4ab7fed1192f0659d53ce50e6f01e4c4f25de
Harshit <bkharshitbk@gmail.com>
2021-10-27 20:34:50 +0530

    setup route/controller for created/participated polls

--- controllers/api/poll.js
+++ controllers/api/poll.js
@@ -1,3 +1,14 @@
+// display all polls page --> then divided in 2 sections: created and participated
 module.exports.polls = async (req, res) => {
+    return res.json([]);
+};
+
+// display created polls
+module.exports.createdPolls = async (req, res) => {
+    return res.json([]);
+};
+
+// display participated polls
+module.exports.participatedPolls = async (req, res) => {
     return res.json([]);
 };
--- routes/api/poll.js
+++ routes/api/poll.js
@@ -6,6 +6,12 @@ const fetchUser = require("../../middlewares/fetchUser");
 
 const pollController = require("../../controllers/api/poll");
 
+// ROUTE 1: DISPLAY ALL POLLS
 router.get("/polls", fetchUser, pollController.polls);
 
+// ROUTE 2: DISPLAY ALL CREATED POLLS
+router.get("/createdPolls", fetchUser, pollController.createdPolls);
+
+// ROUTE 3: DISPLAY ALL PARTICIPATED POLLS
+router.get("/participatedPolls", fetchUser, pollController.participatedPolls);
 module.exports = router;
