22fe208b00a0521e55f86bc9639ff3fb28ffb073
Harshit <bkharshitbk@gmail.com>
2021-11-07 12:32:13 +0530

    route for add poll

--- controllers/api/poll.js
+++ controllers/api/poll.js
@@ -1,12 +1,11 @@
 const Poll = require("../../models/poll");
 
+const { body } = require("express-validator");
+
 // display all polls page --> then divided in 2 sections: created and participated
 module.exports.polls = async (req, res) => {
     const polls = await Poll.find({ poll: req.user.id });
     res.json(polls);
-    // return res.json({
-    //     polls: "no poll available",
-    // });
 };
 
 // display created polls
@@ -17,4 +16,9 @@ module.exports.createdPolls = async (req, res) => {
 // display participated polls
 module.exports.participatedPolls = async (req, res) => {
     return res.json([]);
+};
+
+// create a poll
+module.exports.addPoll = async (req, res) => {
+    return;
 };
--- routes/api/poll.js
+++ routes/api/poll.js
@@ -1,19 +1,31 @@
 const express = require("express");
 const router = express.Router();
 
-const { body } = require("express-validator");
-
 // require middleware - fetchUser to check logged in? {logged in user only}
 const fetchUser = require("../../middlewares/fetchUser");
 
+const { body } = require("express-validator");
+
 const pollController = require("../../controllers/api/poll");
 
-// ROUTE 1: DISPLAY ALL POLLS  *** {GET}->"api/poll/all-polls" ***
+// ROUTE 1: DISPLAY ALL POLLS  *** {GET}->"api/poll/all-polls" *** LOGIN REQUIRED
 router.get("/all-polls", fetchUser, pollController.polls);
 
-// ROUTE 2: DISPLAY ALL CREATED POLLS  *** {GET}->"api/poll/created-polls" ***
+// ROUTE 2: DISPLAY ALL CREATED POLLS  *** {GET}->"api/poll/created-polls" *** LOGIN REQUIRED
 router.get("/created-polls", fetchUser, pollController.createdPolls);
 
-// ROUTE 3: DISPLAY ALL PARTICIPATED POLLS  *** {GET}->"api/poll/participated-polls" ***
+// ROUTE 3: DISPLAY ALL PARTICIPATED POLLS  *** {GET}->"api/poll/participated-polls" *** LOGIN REQUIRED
 router.get("/participated-polls", fetchUser, pollController.participatedPolls);
+
+//ROUTE 4: POST A POLL ***{POST}--> "api/poll/add-poll" LOGIN REQUIRED
+router.post(
+    "/add-poll",
+    [
+        body("description", "Enter a valid description").isLength({ min: 1 }),
+        body("option", "Enter a valid option").isLength({ min: 1 }),
+    ],
+    fetchUser,
+    pollController.addPoll
+);
+
 module.exports = router;
